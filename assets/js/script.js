929d9db238b92b7dc23a0377de816b2e7a876f71
Wolfgang Leithold <wolfleithold@gmail.com>
2024-09-14 16:25:30 -0700

    introduced DOM elements and added api key

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={a4a7f30922e93c33f8e80ace1d6f26cb}
+
+// a4a7f30922e93c33f8e80ace1d6f26cb
+
+const apiKey = "a4a7f30922e93c33f8e80ace1d6f26cb";
+
+// DOM elements
+const cityInput = document.getElementById("city-input");
+const searchBtn = document.getElementById("search-btn");
+const currentWeatherDiv = document.getElementById("weather-details");
+const forecastDiv = document.getElementById("forecast-details");
+const searchHistoryDiv = document.getElementById("search-history");
